d6cb5f6ccaf3bef9b403d691c18aee41d7b0a04b
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2020-12-01 22:54:55 +0530

    Final Changes

--- app/src/main/java/com/example/chromeversionindicator/MainActivity.java
+++ app/src/main/java/com/example/chromeversionindicator/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.chromeversionindicator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        List<PackageInfo> packages = getPackageManager().getInstalledPackages(PackageManager.GET_META_DATA);
+
+        PackageInfo pinfo = null;
+        try
+        {
+            pinfo = getPackageManager().getPackageInfo("com.android.chrome",0);
+        }catch(PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+        try {
+            long verCode = pinfo.versionCode;
+            //getVersionName is Deprecated, instead use versionName
+            String verName = pinfo.versionName;
+            textView.setText(verName);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
     }
 }
